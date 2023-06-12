84a83ef7f957836214a42c1e264e0c65d662830f
alperkizilby <alperkizilby@gmail.com>
2023-06-12 13:11:03 +0300

    Update AccountRegistration.js

--- src/AccountRegistration.js
+++ src/AccountRegistration.js
@@ -1,65 +1,65 @@
 /**
- * Account registration information. Application can query the registration info
- * by calling account.getRegistration().
+ * Hesap kayıt bilgileri. Uygulama, hesap.getRegistration() çağrısı yaparak
+ * kayıt bilgilerini sorgulayabilir.
  */
 export default class AccountRegistration {
-    _status;
-    _statusText;
-    _active;
-    _reason;
+  _status;
+  _statusText;
+  _active;
+  _reason;
 
-    constructor({status, statusText, active, reason}) {
-        this._status = status;
-        this._statusText = statusText;
-        this._active = active;
-        this._reason = reason;
-    }
+  constructor({ status, statusText, active, reason }) {
+    this._status = status;
+    this._statusText = statusText;
+    this._active = active;
+    this._reason = reason;
+  }
 
-    /**
-     * Last registration status code (SIP status codes according to RFC 3261).
-     * If status code is empty, the account is currently not registered. Any other value indicates the SIP
-     * status code of the registration.
-     *
-     * @returns {string|null}
-     */
-    getStatus() {
-        return this._status;
-    }
+  /**
+   * Son kayıt durumu kodu (RFC 3261'e göre SIP durum kodları).
+   * Durum kodu boş ise, hesap şu anda kayıtlı değildir. Herhangi bir diğer değer, kaydın
+   * SIP durum kodunu gösterir.
+   *
+   * @returns {string|null}
+   */
+  getStatus() {
+    return this._status;
+  }
 
-    /**
-     * String describing the registration status.
-     *
-     * @returns {string|null}
-     */
-    getStatusText() {
-        return this._statusText;
-    }
+  /**
+   * Kayıt durumunu açıklayan metin.
+   *
+   * @returns {string|null}
+   */
+  getStatusText() {
+    return this._statusText;
+  }
 
-    /**
-     * Flag to tell whether this account is currently registered
-     * (has active registration session).
-     *
-     * @returns boolean
-     */
-    isActive() {
-        return this._active;
-    }
+  /**
+   * Bu hesabın şu anda kayıtlı olup olmadığını belirten bayrak
+   * (etkin bir kayıt oturumuna sahip mi?).
+   *
+   * @returns boolean
+   */
+  isActive() {
+    return this._active;
+  }
 
-    /**
-     * Reason phrase received.
-     *
-     * @returns {String|null}
-     */
-    getReason() {
-        return this._reason;
-    }
+  /**
+   * Alınan gerekçe ifadesi.
+   *
+   * @returns {String|null}
+   */
+  getReason() {
+    return this._reason;
+  }
 
-    toJson() {
-        return  {
-            status: this._status,
-            statusText: this._statusText,
-            active: this._active,
-            reason: this._reason
-        }
-    }
+  toJson() {
+    return {
+      status: this._status,
+      statusText: this._statusText,
+      active: this._active,
+      reason: this._reason,
+    };
+  }
 }
